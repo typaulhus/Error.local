818959c86df3e339fafaafbc32b2d44f981f4112
Ty Paulhus <typaulhus@gmail.com>
2012-09-13 14:34:21 -0400

    new paragraph

--- about.php
+++ about.php
@@ -9,6 +9,7 @@ Template Name: About
 
 <div class="aboutMain">
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros.</p>
+	<p>new paragraph here</p>
 	<p class="rounded">Full width bar</p>
 </div>
 
